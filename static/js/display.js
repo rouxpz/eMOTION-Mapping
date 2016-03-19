6579da56f530de40c7d1901fabcc38793321e9a2
Roopa Vasudevan <roopakutty@gmail.com>
2016-03-19 18:36:25 +0800

    filtering on map page

--- static/js/display.js
+++ static/js/display.js
@@ -1,8 +1,9 @@
 $("#heading").click(function() {
 	$("#instructions").slideToggle("slow");
+    $("#selection").fadeToggle("slow");
 });
 
-var map = L.map('map').setView([40.7127, -74.0059], 7);
+var map = L.map('map').setView([40.7127, -72.0059], 7);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
@@ -44,16 +45,39 @@ var latlngs = [[nyc, philly], [nyc, boston], [nyc, baltimore]];
 
 
 for (var i = 0; i < totalEmotions.length; i++) {
+    var destID = totalEmotions[i][4];
     if (isNaN(totalEmotions[i][1]) === false) {
     	var loc = L.latLng(totalEmotions[i][1], totalEmotions[i][2]);
-        console.log(loc);
+        // console.log(loc);
     	var popupContent = totalEmotions[i][0];
     	var m = L.circle(loc,
     			5000, 
     			{color: totalEmotions[i][3], 
     			fillColor: totalEmotions[i][3],
-                fillOpacity: 0.5}
+                fillOpacity: 0.5,
+                className: '' + destID + ' emotionMarker'}
     		).addTo(map);
     	m.bindPopup(popupContent, {minWidth: '300', className: 'custom'});
+    }
+}
+
+function showHide(destination) {
+    var eMarkers = document.getElementsByClassName('emotionMarker');
+    for (var i = 0; i < eMarkers.length; i++) {
+
+        if (eMarkers[i].classList.contains(destination)) {
+            console.log("found a match!");
+            eMarkers[i].style.display = "block";
+        } else {
+            console.log("no match");
+            eMarkers[i].style.display = "none";
+        }
+    }
+}
+
+function showAll() {
+    var eMarkers = document.getElementsByClassName('emotionMarker');
+    for (var i = 0; i < eMarkers.length; i++) {
+        eMarkers[i].style.display = "block";
     }
 }
